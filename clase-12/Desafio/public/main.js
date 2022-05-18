56beec84409161072192941694445232283b3fe9
Valentin Kebat <40341497+Valenkebat@users.noreply.github.com>
2022-05-18 20:40:03 -0300

    Ultimo Desafio COmpletado

---
+++ clase-12/Desafio/public/main.js
@@ -0,0 +1,81 @@
+const socket = io.connect()
+
+let username = sessionStorage.getItem("username");
+if (!username) {
+  username = prompt("Ingrese email");
+}
+$("#username").html(username);
+
+
+function renderMesajes(data) {
+  data.forEach((info) => {
+    $("#messages").prepend(`
+      <div>
+          <em class="text-primary fw-bold">${info.author}</em>
+          [<em class="text-danger">${info.time}</em>]: <em class="text-success fst-italic">${info.text}</em>
+      </div>
+    `);
+  });
+}
+
+function renderProductos(data){
+
+  data.forEach((info) => {
+    $("#tabla-productos").prepend(`
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Título</th>
+          <th scope="col">Precio</th>
+          <th scope="col">URL imagen</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <th scope="row">${info.id}</th>
+              <td>${info.title}</td>
+              <td>${info.price}</td>
+              <td>${info.thumbnail}</td>
+          </tr>    
+      </tbody>
+    </table>
+    `);
+  })
+
+}
+
+
+
+socket.on("messages", (data) => {
+  console.log(data);
+  renderMesajes(data);
+});
+
+socket.on('productos', data =>{ 
+  renderProductos(data)
+});
+
+//enviar Mensaje
+$('#myChat').on('submit', e => {
+  e.preventDefault();
+
+  const message = {
+    author: username,
+    text: $("#text").val()
+  };
+
+  socket.emit("new-message", message);
+});
+
+
+//Guardar Producto
+const form = document.getElementById('form-products')
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const data = {title: form[0].value, price: form[1].value, thumbnail: form[2].value}
+    socket.emit("new-product", data);
+    socket.on('productos', data =>{ 
+      renderProductos(data)
+    });
+})
---
+++ clase-12/Desafio/server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const Contenedor = require("./clases");
+const handlebars = require('express-handlebars');
+
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+
+const PORT = 8080
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+const productos = new Contenedor(__dirname + "/data/productos.txt");
+
+const messages = new Contenedor(__dirname + "/data/mensajes.txt");
+
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+      extname: ".hbs",
+      defaultLayout: 'main.hbs',
+      partialsDir: __dirname + "/views/partials",
+    })
+  );
+app.set("views", "./views");
+app.set("views engine", "hbs");
+  
+app.get("/", (req, res) => {
+  return res.render("layouts/main.hbs");
+});
+
+
+app.post("/productos", (req, res) => {
+    productos.save(req.body)
+});
+
+app.get("/productos", (req, res) => {
+  let response = productos.getAll()
+  return response;
+});
+
+/* sockets */
+io.on("connection", (socket) => {
+  socket.emit("messages", messages.getAll());
+  socket.emit("productos", productos.getAll());
+
+  socket.on("new-product", (data) => {
+    productos.save(data)
+    io.sockets.emit("productos", productos.getAll());
+  });
+
+  socket.on("new-message", (data) => {
+    data.time = new Date().toLocaleString();
+    messages.save(data)
+    io.sockets.emit("messages", messages.getAll());
+  });
+});
+
+
+// SERVER
+httpServer.listen(PORT, () => {
+console.log(`Servidor escuchando en el puerto ${httpServer.address().port}`)
+})
+httpServer.on('error', error => console.log(`Error en servidor ${error}`))
+
+
+
+
